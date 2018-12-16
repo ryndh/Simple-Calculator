f994e179d0dd48e7001fbc647a42f30175313ad7
ryanhull <ryhu23@gmail.com>
2018-12-15 19:44:00 -0700

    fixed dot handler, refactored a bit

--- src/components/app.js
+++ src/components/app.js
@@ -5,10 +5,10 @@ import Number from './number';
 
 
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
 
-    this.state={
+    this.state = {
       value: '',
       dot: false,
     }
@@ -17,59 +17,57 @@ export default class App extends Component {
     this.handleClear = this.handleClear.bind(this)
     this.handleEqual = this.handleEqual.bind(this)
     this.handleDot = this.handleDot.bind(this)
-    this.timer = this.timer.bind(this)
-  }
-
-  timer() {
-    console.log(this.state.dot)
-  }
-
-  componentDidMount(){
-    setInterval(this.timer, 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer)
   }
 
   handleClear() {
-    this.setState({value:''});
-    this.setState({dot: false});
+    this.setState({ value: '' });
   }
 
   handleClickNum(e) {
-      this.setState({value: this.state.value + e.target.innerHTML})
+    this.setState({ value: this.state.value + e.target.innerHTML })
   }
 
   handleEqual() {
-    this.setState({value: math.eval(this.state.value)});
-    this.setState({dot: false});
+    this.setState({ value: math.eval(this.state.value).toString() });
   }
 
   handleDot(e) {
-    if (this.state.dot == false && !isNaN(this.state.value[this.state.value.length - 1])) {
-      this.setState({value: this.state.value + e.target.innerHTML});
-      this.setState({dot: true});
+    const ops = ['+', '-', '*', '/']
+    if (ops.some((el) => this.state.value.includes(el))) {
+      for (let item of ops) {
+        if (this.state.value.includes(item)) {
+          let operator = item
+          if (this.state.value.split(operator)[1].includes('.')) {
+            return null
+          } else {
+            this.setState({ value: this.state.value + e.target.innerHTML })
+          }
+        }
+      }
     } else {
-      return null
+      if (this.state.value.includes('.')) {
+        return null
+      } else {
+        this.setState({ value: this.state.value + e.target.innerHTML })
+      }
     }
+
   }
 
   handleClickOp(e) {
     const ops = ['+', '-', '*', '/']
     if (ops.includes(this.state.value[this.state.value.length - 1])) {
-      this.setState({value: this.state.value.slice(0, this.state.value.length - 1) + e.target.innerHTML });
-      this.setState({dot: false});
+      this.setState({ value: this.state.value.slice(0, this.state.value.length - 1) + e.target.innerHTML });
     } else if (this.state.value == '' || this.state.value == 0) {
       return null
     } else {
-      this.setState({value: math.eval(this.state.value) + e.target.innerHTML})
-      this.setState({dot: false});
+      this.setState({ value: math.eval(this.state.value) + e.target.innerHTML })
     }
   }
+
   render() {
-    let someNum = parseInt(this.state.value)
-    let background = {backgroundImage: `linear-gradient(to right, white ${someNum * .1}%, skyblue`}
+    let someNum = parseFloat(this.state.value)
+    let background = { backgroundImage: `linear-gradient(to right, white ${someNum * .1}%, skyblue` }
     return (
       <div className='app-wrap' style={background}>
         <div className='app'>
@@ -77,18 +75,18 @@ export default class App extends Component {
           <h1 className='total'>{this.state.value == '' ? 0 : this.state.value}</h1>
           <div className='calc-wrap'>
             <div className='num-wrap'>
-              <Number click={this.handleClickNum} num={1}/>
-              <Number click={this.handleClickNum} num={2}/>
-              <Number click={this.handleClickNum} num={3}/>
-              <Number click={this.handleClickNum} num={4}/>
-              <Number click={this.handleClickNum} num={5}/>
-              <Number click={this.handleClickNum} num={6}/>
-              <Number click={this.handleClickNum} num={7}/>
-              <Number click={this.handleClickNum} num={8}/>
-              <Number click={this.handleClickNum} num={9}/>
-              <Number click={this.handleDot} num={'.'}/>
-              <Number click={this.handleClickNum} num={0} zero='zero'/>
-            
+              <Number click={this.handleClickNum} num={1} />
+              <Number click={this.handleClickNum} num={2} />
+              <Number click={this.handleClickNum} num={3} />
+              <Number click={this.handleClickNum} num={4} />
+              <Number click={this.handleClickNum} num={5} />
+              <Number click={this.handleClickNum} num={6} />
+              <Number click={this.handleClickNum} num={7} />
+              <Number click={this.handleClickNum} num={8} />
+              <Number click={this.handleClickNum} num={9} />
+              <Number click={this.handleDot} num={'.'} />
+              <Number click={this.handleClickNum} num={0} zero='zero' />
+
             </div>
             <div className='op-wrap'>
               <div onClick={this.handleClear} className='op'>AC</div>
@@ -99,7 +97,7 @@ export default class App extends Component {
               <div onClick={this.handleEqual} className='op'>=</div>
             </div>
           </div>
-              <div className='info'>Gradient Shifts As Total Gets Bigger <br/>(Between -1000 and 1000)</div>
+          <div className='info'>Gradient Shifts As Total Gets Bigger <br />(Between -1000 and 1000)</div>
         </div>
       </div>
     );
